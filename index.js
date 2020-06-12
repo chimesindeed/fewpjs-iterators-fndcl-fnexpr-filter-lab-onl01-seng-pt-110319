044f5f54f9a3c0aafc1f744b900d169ef8637b32
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-06-12 05:30:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ function findMatching(arr, name){
   return arr.filter(el => el.toLowerCase() === name.toLowerCase())
 }
 
-function fuzzyMatch(){}
+function fuzzyMatch(){
+  return arr.filter(el => el.charAt(0) === name.toLowerCase())
+}
 
 function matchName(){}
